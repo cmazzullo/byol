ff610c733da95b49e397906799978781d76292b1
Chris Mazzullo <chris.mazzullo@gmail.com>
2017-11-23 19:41:07 -0500

    map.c seems to be completely solid

--- map.c
+++ map.c
@@ -33,6 +33,46 @@ hash(lval *key)
   return (943287 * h) % ARRAYSIZE;
 }
 
+// List helper functions
+
+/* Remove an lval by key from a list of key/value pairs */
+list *
+list_remove_pair(list *l, lval *k)
+{
+  if (!l) { return l; }
+  char *list_key = get_sym(lval_first(list_first(l)));
+  // if the sym `k` and the sym in the current node are equal:
+  if (strcmp(list_key, get_sym(k)) == 0) {
+    return list_rest(l);
+  } else {
+    return list_cons(list_first(l), list_remove_pair(list_rest(l), k));
+  }
+}
+
+list *
+list_remove(list *l, lval *v)
+{
+  if (!l) { return l; }
+  char *list_key = get_sym(list_first(l));
+  if (strcmp(list_key, get_sym(v)) == 0) {
+    return list_rest(l);
+  } else {
+    return list_cons(list_first(l), list_remove(list_rest(l), v));
+  }
+}
+
+
+lval *
+list_get(list *l, lval *k)
+{
+  if (!l) { return NULL; }
+  if (strcmp(get_sym(lval_first(list_first(l))), get_sym(k)) == 0) {
+    return lval_first(lval_rest(list_first(l)));
+  } else {
+    return list_get(list_rest(l), k);
+  }
+}
+
 map *
 map_new(void)
 {
@@ -72,7 +112,7 @@ void
 map_remove(map *m, lval *key)
 {
   m->keys = list_remove(m->keys, key);
-  m->data[hash(key)] = list_remove(m->data[hash(key)], key);
+  m->data[hash(key)] = list_remove_pair(m->data[hash(key)], key);
 }
 
 /*
@@ -82,23 +122,24 @@ map_remove(map *m, lval *key)
 void
 map_add(map *m, lval *key, lval *val)
 {
-  if (map_contains(m, key)) {map_remove(m, key);}
+  if (map_contains(m, key)) { map_remove(m, key); }
   int h = hash(key);
   // The pair is stored as a 2-element linked list:
   lval *pair = lval_sexp();
-  lval_cons(pair, key);
   lval_cons(pair, val);
+  lval_cons(pair, key);
   m->data[h] = list_cons(pair, m->data[h]);
   m->keys = list_cons(key, m->keys);
 }
 
 /* Wrapper around map_get that returns a bool */
-bool map_contains(map *m, lval *key) { return map_get(m, key) == NULL; }
+bool map_contains(map *m, lval *key) { return map_get(m, key) != NULL; }
 
 lval *
 map_get(map *m, lval *key)
 {
   list *l = m->data[hash(key)];
+  if (!l) { return NULL; }
   return list_get(l, key); // returns NULL if key isn't found/list is NULL
 }
 
@@ -107,11 +148,47 @@ void
 map_print(map *m)
 {
   list *keys = m->keys;
-  printf("{\n");
+  printf("{");
   while (keys) {
     print_lval(list_first(keys));
-    putchar('\n');
+    printf(" : ");
+    print_lval(map_get(m, list_first(keys)));
     keys = list_rest(keys);
+    if (keys) { printf(", "); }
   }
   printf("}\n");
+}
+
+void main() {
+
+  printf("LIST TESTS: \n\n");
+  lval *x = lval_sym("x");
+  lval *y = lval_sym("y");
+  lval *z = lval_sym("z");
+  lval *q = lval_sym("q");
+
+  /* list *l = list_new(x, list_new(y, list_new(z, NULL))); */
+  lval *pair1 = lval_cons(lval_cons(lval_sexp(), x), y);
+  lval *pair2 = lval_cons(lval_cons(lval_sexp(), z), q);
+  list *l = list_new(pair1, NULL);
+  list_print(l); putchar('\n');
+  l = list_cons(pair2, l);
+  list_print(l); putchar('\n');
+
+  printf("MAP TESTS: \n\n");
+  l = list_remove_pair(l, q);
+  list_print(l); putchar('\n');
+  l = list_remove_pair(l, y);
+  list_print(l); putchar('\n');
+  x = lval_sym("x");
+  y = lval_sym("y");
+  z = lval_sym("z");
+  q = lval_sym("q");
+  map *m = map_new();
+  map_add(m, x, y);
+  map_add(m, z, q);
+  map_print(m);
+  map_add(m, z, x);
+  map_print(m);
+  map_delete(m);
 }
